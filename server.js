f18dae9f75543b893b277d82c18842b78eb7891c
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2010-11-07 19:53:25 +0000

    add server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+var sys         = require('sys'),
+    path        = require('path'),
+    http        = require('http'),
+    url         = require('url'),
+    querystring = require('querystring'),
+    paperboy    = require('paperboy')
+
+var PUBLIC = path.join(path.dirname(__filename), 'public');
+
+
+http.createServer(function(req, res) {
+  paperboy
+    .deliver(PUBLIC, req, res)
+    .after(function(status_code) {
+      sys.log('Served Request: ' + status_code + ' ' + req.url)
+    })
+    .otherwise(function() {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write('Not Found');
+      res.end();
+    });
+}).listen(8000, '127.0.0.1');
+
+sys.log('ready at http://localhost:8000/')
